f74ac5464668d91a9f3bc620abdb06c61a28c6e6
Jamison Blackwell <blackwellj1040@gmail.com>
2019-09-24 23:17:30 -0400

    working on comments still

--- client/src/VacationPage.js
+++ client/src/VacationPage.js
@@ -4,11 +4,50 @@ import axios from 'axios';
 export default class VacationPage extends React.Component {
     constructor(props){
         super(props)
+        this.state = {
+            comment: '',
+            comments: '',
+           user_id: Number(this.props.match.params.id)
+        }
+        console.log(this.props)
+        console.log(this.props.match.params.id)
     }
 
 
+    componentDidMount(){
+        
+        const token = localStorage.getItem('token')
+        axios
+        .get('https://vacation-planner-bw.herokuapp.com/api/vacations/comments')
+            .then(res => {
+                this.setState({comments: res.data})
             
+            })
+            .catch(err => {
+                console.log(err)
+            })
+            
+    }
+
+     
+    changeHandler = (e) => {
+        e.preventDefault();
+        this.setState({[e.target.name]: e.target.value})
+    }   
     
+    addComment = (e) => {
+        e.preventDefault();
+        const {comment, user_id} = this.state
+        axios
+        .post('https://vacation-planner-bw.herokuapp.com/api/vacations/comments', {comment, user_id})
+            .then(res => {
+                console.log(res)
+            })
+            .catch(err => {
+                console.log(err)
+            })
+          
+    }
 
     render() {
         const vacation = this.props.vacations.find(i => String(i.id) === this.props.match.params.id)
@@ -21,6 +60,16 @@ if (!vacation) {
                 <p>{vacation.username}</p>
                 <p>{vacation.destination}</p>
                 <p>{vacation.description}</p>
+                <form onSubmit={this.addComment}>
+                    <input 
+                        placeholder='comment'
+                        value={this.state.comment}
+                        onChange={this.changeHandler}
+                        name='comment'
+
+                    />
+                    <button type='submit'>Add</button>
+                </form>
             </div>
         )
     }
