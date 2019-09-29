082d1acbfc9f63da508d642cae9f3770f04ddc36
Jamison Blackwell <blackwellj1040@gmail.com>
2019-09-29 11:36:28 -0400

    message endpoint is fixed

--- client/src/vacations.js
+++ client/src/vacations.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { Link } from 'react-router-dom';
 import NavBar from './NavBar';
 import BoardWalk from './img/khachik-simonian-nXOB-wh4Oyc-unsplash (1).jpg';
-import {Button, Image, fluid} from 'react-bootstrap';
+import {ButtonToolbar, DropdownButton, Dropdown} from 'react-bootstrap';
 import './Vacations.css';
 import $ from 'jquery';
 import ReactImageMagnify from 'react-image-magnify';
@@ -57,25 +57,108 @@ this.state = {
         const userId = this.state.id
         console.log(userId)
         return (
-            <div>
-                
-        <NavBar />
+            <div>        
+                <NavBar />
+                <div className='container'>
+                    <ButtonToolbar className='buttons'>
+                        {['Destination',].map(
+                            variant => (
+                            <DropdownButton
+                                title={variant}
+                                variant={variant.toLowerCase()}
+                                id={`dropdown-variants-${variant}`}
+                                key={variant}
+                                className='buttn'
+                            >
+                                <Dropdown.Item eventKey="1">Action</Dropdown.Item>
+                                <Dropdown.Item eventKey="2">Another actiosn</Dropdown.Item>
+                                <Dropdown.Item eventKey="3" active>
+                                Active Item
+                                </Dropdown.Item>
+                                <Dropdown.Divider />
+                                <Dropdown.Item eventKey="4">Separated link</Dropdown.Item>
+                            </DropdownButton>
+                            ),
+                        )}
+                        {[ 'Price' ].map(
+                            variant => (
+                            <DropdownButton
+                                
+                                title={variant}
+                                variant={variant.toLowerCase()}
+                                id={`dropdown-variants-${variant}`}
+                                key={variant}
+                                className='buttn'
+
+                            >
+                                <Dropdown.Item eventKey="1">Action</Dropdown.Item>
+                                <Dropdown.Item eventKey="2">Another actiosn</Dropdown.Item>
+                                <Dropdown.Item eventKey="3" active>
+                                Active Item
+                                </Dropdown.Item>
+                                <Dropdown.Divider />
+                                <Dropdown.Item eventKey="4">Separated link</Dropdown.Item>
+                            </DropdownButton>
+                            ),
+                        )}
+                         {[ 'Cost' ].map(
+                            variant => (
+                            <DropdownButton
+                                title={variant}
+                                variant={variant.toLowerCase()}
+                                id={`dropdown-variants-${variant}`}
+                                key={variant}
+                                className='buttn'
+
+                            >
+                                <Dropdown.Item eventKey="1">Action</Dropdown.Item>
+                                <Dropdown.Item eventKey="2">Another actiosn</Dropdown.Item>
+                                <Dropdown.Item eventKey="3" active>
+                                Active Item
+                                </Dropdown.Item>
+                                <Dropdown.Divider />
+                                <Dropdown.Item eventKey="4">Separated link</Dropdown.Item>
+                            </DropdownButton>
+                            ),
+                        )}
+                         {[ 'Weather' ].map(
+                            variant => (
+                            <DropdownButton
+                                title={variant}
+                                variant={variant.toLowerCase()}
+                                id={`dropdown-variants-${variant}`}
+                                key={variant}
+                                                                className='buttn'
+
+                            >
+                                <Dropdown.Item eventKey="1">Action</Dropdown.Item>
+                                <Dropdown.Item eventKey="2">Another actiosn</Dropdown.Item>
+                                <Dropdown.Item eventKey="3" active>
+                                Active Item
+                                </Dropdown.Item>
+                                <Dropdown.Divider />
+                                <Dropdown.Item eventKey="4">Separated link</Dropdown.Item>
+                            </DropdownButton>
+                            ),
+                        )}
+                    </ButtonToolbar>
+                </div>
                 <div className='vacation-content-container'>
-                    {this.props.vacations.map(vac => {
-                    return  <div>
-                    <div onMouseOver={this.hover} className='vacation-content'>
-                        <Link style={{ color: 'inherit', textDecoration: 'inherit'}} exact to ={`/vacationpage/${vac.id}`}>
-                            <img className='vacation-img' src={ BoardWalk }/>
-                            <div>
-                            <p>{vac.destination}</p> 
-                            <p>${vac.cost}</p>
-                            <p>{vac.description}Lorem ipsum dolor sit amet, duo at nominati principes, falli muner ipsum dolor sit amet, duo at nominati principes, falli muner</p>
-                            <p>{vac.start_date}</p>
-                            </div>
-                        </Link>
-                    </div>
-                    </div>
-                    })}
+                        {this.props.vacations.map(vac => {
+                        return  <div>
+                        <div onMouseOver={this.hover} className='vacation-content'>
+                            <Link style={{ color: 'inherit', textDecoration: 'inherit'}} exact to ={`/vacationpage/${vac.id}`}>
+                                <img className='vacation-img' src={ BoardWalk }/>
+                                <div>
+                                <p>{vac.destination}</p> 
+                                <p>${vac.cost}</p>
+                                <p>{vac.description}Lorem ipsum dolor sit amet, duo at nominati principes, falli muner ipsum dolor sit amet, duo at nominati principes, falli muner</p>
+                                <p>{vac.start_date}</p>
+                                </div>
+                            </Link>
+                        </div>
+                        </div>
+                        })}
                 </div>
             </div>
         )
